0d9e424ce3d88c09ce27445fde705d6ddaae72ab
Golfing7 <atroo@me.com>
2024-01-29 12:20:30 -0500

    Cleanup lang enum

--- KCommon/src/test/java/com/golfing8/kcommon/module/test/TestModule.java
+++ KCommon/src/test/java/com/golfing8/kcommon/module/test/TestModule.java
@@ -1,35 +1,12 @@
 package com.golfing8.kcommon.module.test;
 
-import com.golfing8.kcommon.config.lang.LangEnum;
-import com.golfing8.kcommon.config.lang.Message;
 import com.golfing8.kcommon.module.Module;
 import com.golfing8.kcommon.module.ModuleInfo;
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
 
 @ModuleInfo(
         name = "test"
 )
 public class TestModule extends Module {
-
-    public enum Messages implements LangEnum {
-        TEST_MESSAGE(new Message("Some test message"))
-        ;
-        private Message message;
-        Messages(Message message) {
-            this.message = message;
-        }
-
-        @Override
-        public Message getMessage() {
-            return message;
-        }
-
-        @Override
-        public void setMessage(Message message) {
-            this.message = message;
-        }
-    }
     @Override
     public void onEnable() {
 
