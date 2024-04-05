01bcc8702eaea7c000526aabcca84a059c053b50
Golfing8 <atroo@me.com>
2024-04-05 19:12:55 -0400

    Add util methods for command arguments

--- KCommon/src/main/java/com/golfing8/kcommon/command/argument/CommandArgument.java
+++ KCommon/src/main/java/com/golfing8/kcommon/command/argument/CommandArgument.java
@@ -3,13 +3,12 @@ package com.golfing8.kcommon.command.argument;
 import com.golfing8.kcommon.util.StringUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.var;
 
-import java.util.Collection;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Represents an argument for a command.
@@ -51,5 +50,25 @@ public class CommandArgument<A> {
         return new CommandArgument<>("A type of " + StringUtil.capitalize(enumm.getSimpleName()), argumentContext -> allStrings.keySet(),
                 argumentContext -> allStrings.containsKey(argumentContext.getArgument().toUpperCase()),
                 argument -> (T) allStrings.get(argument.toUpperCase()));
+    }
+
+    public static <T> CommandArgument<T> fromMap(String typeName, Map<String, T> map) {
+        Map<T, String> reverseMap = new HashMap<>();
+        for (var entry : map.entrySet()) {
+            reverseMap.put(entry.getValue(), entry.getKey());
+        }
+        return fromCollection(typeName, map.values(), map::get, reverseMap::get);
+    }
+
+    public static <T> CommandArgument<T> fromCollection(String typeName, Collection<T> coll, Function<String, T> fromString) {
+        return fromCollection(typeName, coll, fromString, Objects::toString);
+    }
+
+    public static <T> CommandArgument<T> fromCollection(String typeName, Collection<T> coll, Function<String, T> fromString, Function<T, String> toString) {
+        Set<String> completions = coll.stream().map(toString).collect(Collectors.toSet());
+        return new CommandArgument<>("A type of " + typeName,
+                argumentContext -> completions,
+                argumentContext -> completions.contains(argumentContext.getArgument()),
+                fromString);
     }
 }
