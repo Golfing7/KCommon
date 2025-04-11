4d8134fe7ddd3ad2a94d741ac767c79b97cca1e0
Golfing7 <atroo@me.com>
2025-04-11 09:05:41 -0400

    Update logger

--- KCommon/src/main/java/com/golfing8/kcommon/module/ModuleLogger.java
+++ KCommon/src/main/java/com/golfing8/kcommon/module/ModuleLogger.java
@@ -5,17 +5,9 @@ import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 public class ModuleLogger extends Logger {
-    private String moduleName;
     protected ModuleLogger(Module module) {
-        super(module.getClass().getCanonicalName(), null);
-        moduleName = module.getModuleName();
+        super(module.getClass().getSimpleName(), null);
         this.setParent(module.getPlugin().getLogger());
         this.setLevel(Level.ALL);
-    }
-
-    @Override
-    public void log(LogRecord record) {
-        record.setMessage("[" + this.moduleName + "] " + record.getMessage());
-        super.log(record);
     }
 }
