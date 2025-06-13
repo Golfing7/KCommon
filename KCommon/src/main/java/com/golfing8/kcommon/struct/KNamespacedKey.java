dfe3f8af17f0e2272f78eed1d88080bc3e2d87f8
Golfing8 <atroo@me.com>
2025-06-13 08:28:42 -0400

    Remove toBukkit method

--- KCommon/src/main/java/com/golfing8/kcommon/struct/KNamespacedKey.java
+++ KCommon/src/main/java/com/golfing8/kcommon/struct/KNamespacedKey.java
@@ -2,7 +2,6 @@ package com.golfing8.kcommon.struct;
 
 import com.google.common.base.Preconditions;
 import net.kyori.adventure.key.Key;
-import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -190,10 +189,6 @@ public class KNamespacedKey implements Key {
     
     public String getKey() {
         return this.key;
-    }
-
-    public NamespacedKey toBukkit() {
-        return new NamespacedKey(this.namespace, this.key);
     }
 
     @Override
