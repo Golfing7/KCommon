f2779691e80a338d70d07912025113a6654fe3f4
Golfing8 <atroo@me.com>
2025-11-25 09:22:32 -0500

    Add item selection menu

---
+++ KCommon/src/main/java/com/golfing8/kcommon/menu/helper/ItemSelectionMenu.java
@@ -0,0 +1,68 @@
+package com.golfing8.kcommon.menu.helper;
+
+import com.cryptomorin.xseries.XMaterial;
+import com.golfing8.kcommon.KCommon;
+import com.golfing8.kcommon.menu.Menu;
+import com.golfing8.kcommon.menu.MenuBuilder;
+import com.golfing8.kcommon.menu.MenuShapeType;
+import com.golfing8.kcommon.menu.PlayerMenuContainer;
+import com.golfing8.kcommon.struct.helper.promise.Promise;
+import com.golfing8.kcommon.struct.item.ItemStackBuilder;
+import com.golfing8.kcommon.util.ItemUtil;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+public class ItemSelectionMenu extends PlayerMenuContainer {
+    /** The promise of completion */
+    @Getter
+    private final Promise<@Nullable ItemStack> promise;
+    private boolean answering = false;
+
+    public ItemSelectionMenu(Player player) {
+        super(player);
+
+        this.promise = Promise.empty();
+    }
+
+    @Override
+    protected Menu loadMenu() {
+        MenuBuilder builder = MenuBuilder.builder()
+                .title("&aSelect an Item")
+                .shapeType(MenuShapeType.HOPPER)
+                .postCloseRunnable((event) -> {
+                    if (answering)
+                        return;
+
+                    if (!promise.isDone())
+                        promise.supply(null);
+                });
+        ItemStackBuilder item = new ItemStackBuilder()
+                .material(XMaterial.BLACK_STAINED_GLASS_PANE)
+                .name("&eSelect an item from your inventory");
+        for (int i = 0; i < 5; i++) {
+            builder.setAt(i, item.buildCached());
+        }
+        builder.bottomClickAction(event -> {
+            if (answering)
+                return;
+
+            if (!event.getClick().isLeftClick() && !event.getClick().isRightClick())
+                return;
+
+            ItemStack clicked = event.getCurrentItem();
+            if (ItemUtil.isAirOrNull(clicked))
+                return;
+
+            answering = true;
+            Bukkit.getScheduler().runTask(KCommon.getInstance(), () -> {
+                event.getWhoClicked().closeInventory();
+                promise.supply(clicked);
+            });
+        });
+
+        return builder.buildSimple();
+    }
+}
